b090811bd2e9b347569a05d424ae0f1cf49a4e83
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-02-01 01:38:05 +1100

    .

--- App.tsx
+++ App.tsx
@@ -8,50 +8,48 @@ import {
   FlatList,
   Dimensions,
   SafeAreaView,
-  ScrollView
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
-import Header from './components/header';
-import Homemain from './components/homemain';
+
+import Home from './screens/home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Albums from './screens/albums';
 //import { Text } from 'react-native-reanimated/lib/typescript/Animated';
 
+const Stack = createStackNavigator();
 
+const openDrawer = () => {}
 
 
 export default function App() {
   //fetchAlbums
   //console.log("Testing the vibes")
   return (
-    //console.log("go there"),
-    <SafeAreaView style={styles.top}>
-      <ScrollView>
-      <Header/>
-      <Image style={styles.homeimg} source={require('./assets/oliver-mtukudzi.jpg')}/>
-      <Image style={styles.arrowimg} source={require('./assets/arrow.png')} />
-      <View style={styles.main}>
-        <Text style={styles.abouthead}>About Oliver Mtukudzi</Text>
-        <Text style={styles.aboutpara}>Oliver Mtukudzi, born on September 22, 1952, in Highfield, a suburb of Harare, Zimbabwe, emerged as one of the most influential and beloved figures in Zimbabwean music. Fondly known as "Tuku," he grew up in a culturally rich environment, surrounded by the vibrant sounds and rhythms of traditional Shona music.</Text>
-        <Text style={styles.aboutpara}>
-        Mtukudzi's journey in music began at an early age, and he quickly mastered various instruments, including the guitar, which would become his signature instrument. His exceptional talent and dedication to his craft soon gained recognition, and by the 1970s, he had established himself as a prominent artist in the Zimbabwean music scene.
-        </Text>
-        <Text style={styles.aboutpara}>
-        Beyond the realm of music, Mtukudzi became a cultural icon and a source of inspiration for many Zimbabweans. His music, characterized by profound lyrics and a fusion of traditional rhythms with modern influences, resonated deeply with the people. He addressed social issues, love, and the complexities of life in Zimbabwe, earning him a special place in the hearts of his fellow countrymen.
-        </Text>
-        <Text style={styles.aboutpara}>
-        Oliver Mtukudzi's significance goes beyond his musical prowess; he became a unifying force in Zimbabwe, fostering a sense of national identity and pride through his compositions. His songs often carried messages of hope, resilience, and the celebration of cultural heritage.
-        </Text>
-        <Text style={styles.aboutpara}>
-        Internationally, Mtukudzi transcended the boundaries of Zimbabwean music, captivating audiences worldwide with his unique sound. He collaborated with artists from diverse backgrounds, contributing to the global appreciation of African music. His influence extended beyond the music industry, making him a respected ambassador for Zimbabwean culture on the international stage.
-        </Text>
-        <Text style={[styles.aboutpara, {paddingBottom: 70}]}>
-        Oliver Mtukudzi's legacy endures not only through his extensive discography but also through the countless lives he touched with his music. He passed away on January 23, 2019, leaving behind a profound impact on the world of music and a lasting imprint on the hearts of Zimbabweans, who continue to celebrate his contributions to their cultural heritage.
-        </Text>
-
-      </View>
-      </ScrollView>
-       
-    </SafeAreaView>
-    
-
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home}  options={{
+          title: 'HOME',
+          headerStyle: {
+            backgroundColor: 'black', // Customize the header background color
+          },
+          headerTitleStyle: {
+            color: 'white', // Customize the header title color
+          },
+          headerLeft: () => (
+            <TouchableOpacity onPress={openDrawer}>
+              {/* Your custom drawer button or image */}
+              <Image
+                source={require('./assets/menu.png')}
+                style={{ width: 30, height: 30, marginLeft: 10 }}
+              />
+            </TouchableOpacity>
+          ),
+        }}/>
+      <Stack.Screen name="Albums" component={Albums} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
   
 }
--- screens/albums.js
+++ screens/albums.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { View } from "react-native";
 
 export default function Albums(){
-    
+    return(
+        <View></View>
+    );
 }
